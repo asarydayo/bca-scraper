fb4141024793070576dae18a3af5bddd0857cbf3
Asary <asary.info@gmail.com>
2021-09-25 21:21:00 +0700

    🔥 Removed 👷 Added intergrate.yml

--- src/modules/auth/route.ts
+++ src/modules/auth/route.ts
@@ -2,12 +2,10 @@ import { Router } from "express";
 import auth from "common/middlewares/auth";
 const router = Router();
 
-import forgot from "./forgot";
 import login from "./login";
 import me from "./me";
 
 router.post("/login", login);
-router.post("/forget", forgot);
 router.get("/me", [auth], me);
 
 export default router;
